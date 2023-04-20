190d2414e24135c1a5015ace3dad16e784600784
daniel <daniel.p.henricks@vanderbilt.edu>
2023-04-20 13:08:42 -0500

    changed code 4/20/23

--- SyringePump.ino
+++ SyringePump.ino
@@ -3,7 +3,7 @@
 AccelStepper stepper(AccelStepper::DRIVER, 3, 2);
 // pin 3 is the step input, pin 2 is the direction input
 
-float flowRate = 10.0;
+float flowRate = 100;
 // the current flow rate of the pump
 
 int red_LED = 11;
@@ -14,23 +14,18 @@ int green_LED = 10;
 int button = 5;
 // the pin for the first button
 
-int endButton = 6;
+int endButton = 8;
 // the button that detects if the pump has reached the end of the cycle
+
+int clockwiseButton = 6;
+int ccwButton = 7;
 
 /* ### BONUS QUESTIONS 
 
   Question 1: Two buttons
-  int clockwiseButton = 7;
-  int ccwButton = 8;
+ 
 
-  if(digitalRead(clockwiseButton) == LOW){
-    stepForward();
-  }
-  if(digitalRead(ccwButton) == LOW){
-    stepBackward();
-  }
-  
-  Potentiometer:
+  Question 2: Potentiometer
   in the loop:
   int sensorValue = analogRead(A0) // (the port)
   flowRate = sensorValue / 20.0;
@@ -46,25 +41,33 @@ void setup() {
   pinMode(blue_LED, OUTPUT);
   pinMode(green_LED, OUTPUT);
   pinMode(button, INPUT_PULLUP);
-  pinMode(endButton, INPUT_PULLUP);
+  pinMode(clockwiseButton, INPUT_PULLUP);
+  stepper.setMaxSpeed(1000);
   stepper.setSpeed(flowRate * 3 / 8);
   // 3/8 was calculated by 1 minute/60 seconds * 180 steps/8 millimeters 
   
 }
 
 void loop() {
-  // if the button is pressed and the pump is not at the end of the cycle
-  if(digitalRead(button) == LOW && !digitalRead(endButton)){
+
+  if(digitalRead(clockwiseButton) == LOW){ //not 100% working yet
+    analogWrite(blue_LED, 255);
+    analogWrite(green_LED, 0);
+    analogWrite(red_LED, 0);
+    stepper.runSpeed();
+    Serial.println("clockwise");
+  }
+  if(digitalRead(button) == LOW){
     analogWrite(green_LED, 255);
     analogWrite(red_LED, 0);
+    analogWrite(blue_LED, 0);
     stepper.runSpeed(); // you should step now
+    Serial.println("main button");
   }
-  else if(!digitalRead(endButton)){ // pause because you are not at the end yet
+  else { // pause because you are not at the end yet
     analogWrite(red_LED, 255);
     analogWrite(green_LED, 255);
+    analogWrite(blue_LED, 0);
   }
-  else {
-    analogWrite(green_LED, 0);
-    analogWrite(red_LED, 255); // you have reached the end of the cycle
-  }
+  
 }
