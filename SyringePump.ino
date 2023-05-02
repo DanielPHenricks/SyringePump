6171cd52eac33a0ca819221b6481213f7783b10f
daniel <daniel.p.henricks@vanderbilt.edu>
2023-05-02 15:10:25 -0500

    Working iteration #1

--- SyringePump.ino
+++ SyringePump.ino
@@ -1,11 +1,16 @@
 #include <AccelStepper.h>
+#include <LiquidCrystal.h>// include the library code
+
+LiquidCrystal lcd(1, 4, 6, 7, 12, 13);
+float syringeSize = 5.0; // the capacity of the syringe, in mL.
 
 AccelStepper stepper(AccelStepper::DRIVER, 3, 2);
 // pin 3 is the step input, pin 2 is the direction input
 
-float flowRate = 100;
+float flowRate = (6 * 10)/3;
 // the current flow rate of the pump
 
+// adjust for microstepping
 int red_LED = 11;
 int blue_LED = 9;
 int green_LED = 10;
@@ -17,67 +22,93 @@ int button = 5;
 int endButton = 8;
 // the button that detects if the pump has reached the end of the cycle
 
-int clockwiseButton = 6;
-int ccwButton = 7;
-
-/* ### BONUS QUESTIONS 
-
-  Question 1: Two buttons
- 
-
-  Question 2: Potentiometer
-  in the loop:
-  int sensorValue = analogRead(A0) // (the port)
-  flowRate = sensorValue / 20.0;
-  // convert the sensorVal to a scaled value: (0-1023) -> (0-50) -> divide by 20 to approximate
-  Serial.println(flowRate);
-  delay(1); // is this necessary?
-
-  ### END BONUS QUESTIONS*/
-  
+bool isPaused = false;
+unsigned long startPause = 0;
+unsigned long elapsedPause = 0;
 void setup() {
-  Serial.begin(9600);
   pinMode(red_LED, OUTPUT);
   pinMode(blue_LED, OUTPUT);
   pinMode(green_LED, OUTPUT);
   pinMode(button, INPUT_PULLUP);
   pinMode(endButton, INPUT_PULLUP);
-  //pinMode(clockwiseButton, INPUT_PULLUP);
   stepper.setMaxSpeed(1000);
-  stepper.setSpeed(flowRate * 3 / 8);
-  // 3/8 was calculated by 1 minute/60 seconds * 180 steps/8 millimeters 
-  
+  stepper.setSpeed(flowRate * 50 / 3);
+  lcd.begin(20, 4);
 }
 
 void loop() {
+  int timeRemaining = ceil(syringeSize*60/flowRate*6.7 - millis()/1000 + elapsedPause/1000);
+    timeRemaining = max(timeRemaining, 0);
+    lcd.setCursor(0,0);  
+    lcd.print("**Team Concoction**");
 
-  // if(digitalRead(clockwiseButton) == LOW){ //not 100% working yet
-  //   analogWrite(blue_LED, 255);
-  //   analogWrite(green_LED, 0);
-  //   analogWrite(red_LED, 0);
-  //   stepper.runSpeed();
-  //   Serial.println("clockwise");
-  // }
+    lcd.setCursor(0,1);
+    lcd.print("Flow rate:");
+
+    lcd.setCursor(11,1);
+    lcd.print((int)flowRate);
+
+    lcd.setCursor(0, 2);
+    lcd.print("Time until end (s): ");
+
+    
+    if(!isPaused){
+      lcd.setCursor(0,3);
+      if(timeRemaining >= 100){
+
+      }
+      else if (timeRemaining >= 10){
+        lcd.print(" ");
+      }
+      else {
+        lcd.print("  ");
+      }
+      lcd.print(timeRemaining);
+    }
+    else {
+      int currTime = ceil(syringeSize*60/flowRate*6.7 - startPause/1000 + elapsedPause/1000);
+      lcd.setCursor(0,3);
+      if(currTime >= 100){
+
+      }
+      else if (currTime >= 10){
+        lcd.print(" ");
+      }
+      else {
+        lcd.print("  ");
+      }
+      lcd.print(max(currTime, 0));
+    }
   
   if(digitalRead(endButton) == LOW){ // you have reached the end
-    analogWrite(green_LED, 255);
-    analogWrite(red_LED, 127);
+    if(!isPaused){
+        isPaused = true;
+        startPause = millis();
+    }
+    analogWrite(green_LED, 0);
+    analogWrite(red_LED, 255);
     analogWrite(blue_LED, 0);
+    
   }
   else {
     if(digitalRead(button) == LOW){ // you are not at the end sensor
-    analogWrite(green_LED, 255);
-    analogWrite(red_LED, 0);
-    analogWrite(blue_LED, 0);
-    stepper.runSpeed(); // you should step now
-    Serial.println("main button");
-    }
+      if(isPaused){
+        isPaused = false;
+        elapsedPause += millis() - startPause; 
+      }
+      analogWrite(green_LED, 255);
+      analogWrite(red_LED, 0);
+      analogWrite(blue_LED, 0);
+      stepper.runSpeed(); // you should step now
+      }
     else { // pause because you are not at the end yet
+      if(!isPaused){
+        isPaused = true;
+        startPause = millis();
+      }
       analogWrite(red_LED, 255);
       analogWrite(green_LED, 127);
       analogWrite(blue_LED, 0);
     }
   }
-  
-  
 }
